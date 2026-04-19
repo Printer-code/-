7a5fa3600e582c88ee04baab9faabb4d79e3b43c
Printer-code <2747224159@qq.com>
2026-04-19 23:52:02 +0800

    Add files via upload

---
+++ build/CMakeFiles/just_test_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/just_test_autogen"
+  "just_test_autogen/mocs_compilation.cpp"
+  "just_test_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/just_test_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
