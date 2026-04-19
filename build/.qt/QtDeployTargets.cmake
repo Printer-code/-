246beaaceed87534031802c3ed08906c1b1c5abe
Printer-code <2747224159@qq.com>
2026-04-19 23:22:30 +0800

    Add files via upload

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Qt_test01_FILE C:/Qt_test01/build/Qt_test01.exe)
+set(__QT_DEPLOY_TARGET_Qt_test01_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Qt_test01_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
