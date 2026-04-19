378d66f6192664605067d65fa38e7267b6258fb3
Printer-code <2747224159@qq.com>
2026-04-19 23:38:35 +0800

    Add files via upload

---
+++ build/Qt_test01_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Qt_test01_autogen"
+  "Qt_test01_autogen/mocs_compilation.cpp"
+  "Qt_test01_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Qt_test01_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
